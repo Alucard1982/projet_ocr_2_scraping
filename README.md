5294ccdf2eb41ff9bac9f18eb8ec65abcb8bbd5b
Florent <florentpeyre@hotmail.fr>
2020-11-21 00:54:15 +0100

    final commit, programme_ok, commentaire et readMe

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # PROJET_OCR_2_SCRAPING
+Bonjour, 
+pour lancer le projet dans l'invit de commande:
+-créer le virtual env : python -m venv venv (à la racine du projet)
+-switcher sur le virtual env : source venv/Scripts/activate ou venv\Scripts\activate en fonction du os (à la racine de porjet)
+-récupérer les modules du fichier requirements.txt sur votre venv : pip install -r requirements.txt(à la racine du projet)
+-lancer le programme : python main.py (à la racine du projet)
+
+Enjoy !!!
--- data_recovery.py
+++ data_recovery.py
@@ -9,19 +9,18 @@ import shutil
 def scraping_book_description_and_img(dic_name_and_url):
     """
     fonction qui va créer un dossier par categorie, créer un fichier csv dans le dossier
-     categorie avec les données de chaques book par categorie et un dossier img dans chaque dossier categorie
+    categorie avec les données de chaques book par categorie et un dossier img dans chaque dossier categorie
     avec les images et titres de chaque book par categorie.
-
-     :param le dictionnaire qui regroupe le nom des categories et les urls par categories
+    :param: le dictionnaire qui regroupe le nom des categories et les urls par categories
      """
-    #pour chaque categories
+    # pour chaque categories
     for key, values in dic_name_and_url.items():
-        #creation des dossiers
+        # creation des dossiers
         os.makedirs(key, exist_ok=True)
         os.makedirs("C:\\Users\\marie\\PycharmProjects\\projetOcr2\\" + key + "\\img", exist_ok=True)
-        #creation du fichier csv
+        # creation des fichier csv
         with open(key + "/description_book_" + key + ".csv", 'w', encoding='utf-8') as file:
-            #pour chaque urls dans la categorie on scrap et récup les données
+            # pour chaque urls dans la categorie on scrap et récup les données
             for url_book in values:
                 url = url_book
                 response = requests.get(url)
@@ -32,11 +31,11 @@ def scraping_book_description_and_img(dic_name_and_url):
                     image = soup.find('div', {'class': 'item active'}).find('img')
                     src = image['src']
                     name_image = image['alt']
-                    #ici on enleve les caractères spéciaux
+                    # ici on enleve les caractères spéciaux
                     real_name_image = ''.join(e for e in name_image if e.isalnum())
                     base_url = "http://books.toscrape.com/"
                     url_image = urljoin(base_url, src)
-                    #partie pour récup l'image et l'enregistrer dans le dossier img
+                    # partie pour récup l'image et l'enregistrer dans le dossier img
                     responses = requests.get(url_image, stream=True)
                     if responses.ok:
                         files = open("C:\\Users\\marie\\PycharmProjects\\projetOcr2\\" + key + "\\img\\"
@@ -46,7 +45,7 @@ def scraping_book_description_and_img(dic_name_and_url):
                         del responses
                     list_book = soup.find("table", {"class": 'table-striped'}).find_all('td')
                     del response
-                    #dictionnaire des données d'un book
+                    # dictionnaire des données d'un book
                     dic_book = {'product_page_url': url,
                                 'universal_product_code': list_book[0].text,
                                 'category': list_book[1].text,
@@ -58,7 +57,7 @@ def scraping_book_description_and_img(dic_name_and_url):
                                 'review_rating': list_book[6].text,
                                 'url_image': url_image
                                 }
-                #on ecrit les données du book dans le fichier csv             }
+                # on ecrit les données du book dans le fichier csv             }
                 for clef, valeurs in dic_book.items():
                     file.write(clef + ':' + valeurs + '\n')
                 file.write('\n\n')
@@ -68,13 +67,13 @@ def scraping_book_description_and_img(dic_name_and_url):
 
 def scraping_book_by_categorie(urls_category):
     """
-    prend en parametre d'entrer une list des urls des tte la categories
     fonction qui permet de récuperer les urls de tout les book par catégories
+    :param: une list des urls des tte la categories
     :return:une list avec pour chaque list de categorie ses urls respectives(des listes dans une liste)
     """
     list_link_book = []
     list_link_book_by_category = []
-    #on boucle sur chaque categories
+    # on boucle sur chaque categories
     for url_category in urls_category:
         url = url_category
         response = requests.get(url)
@@ -93,32 +92,32 @@ def scraping_book_by_categorie(urls_category):
                 base_url = "http://books.toscrape.com"
                 real_link_book = urljoin(base_url, '/catalogue' + link_book[8:])
                 list_link_book.append(real_link_book)
-        try:
-            # tant que qu'il ya un button next on recup l'url de la page suivante et on scrap la page
-            # jusqu'a qu'il n'y ai plus de next (pagination)
-            while button_next:
-                new_url = url[:-10]
-                #ici ca permet de voir si la condition de la boucle est bonne avec button_next
-                button_next = soup.find('li', {'class': 'next'}).find('a')
-                href = button_next['href']
-                #nouvelle page nouvelle url qu'on récup
-                url = urljoin(new_url, href)
-                response = requests.get(url)
-                soup = BeautifulSoup(response.text, "html.parser")
-                urls_book = soup.findAll('div', {'class': 'image_container'})
-                del response
-                #on cherche tte les urls de book par page
-                for url_book in urls_book:
-                    a = url_book.find('a')
-                    link_book = a['href']
-                    base_url = "http://books.toscrape.com"
-                    real_link_book = urljoin(base_url, '/catalogue' + link_book[8:])
-                    list_link_book.append(real_link_book)
-        except:
-            pass
-        list_link_book_by_category.append(list_link_book)
-        list_link_book = []
-
+            try:
+                # tant que qu'il ya un button next on recup l'url de la page suivante et on scrap la page
+                # jusqu'a qu'il n'y ai plus de next (pagination)
+                while button_next:
+                    new_url = url[:-10]
+                    # ici ca permet de voir si la condition de la boucle est bonne avec button_next
+                    button_next = soup.find('li', {'class': 'next'}).find('a')
+                    href = button_next['href']
+                    # nouvelle page nouvelle url qu'on récup
+                    url = urljoin(new_url, href)
+                    response = requests.get(url)
+                    if response.ok:
+                        soup = BeautifulSoup(response.text, "html.parser")
+                        urls_book = soup.findAll('div', {'class': 'image_container'})
+                        del response
+                        # on cherche tte les urls de book par page
+                        for url_book in urls_book:
+                            a = url_book.find('a')
+                            link_book = a['href']
+                            base_url = "http://books.toscrape.com"
+                            real_link_book = urljoin(base_url, '/catalogue' + link_book[8:])
+                            list_link_book.append(real_link_book)
+            except:
+                pass
+            list_link_book_by_category.append(list_link_book)
+            list_link_book = []
     return list_link_book_by_category
 
 
