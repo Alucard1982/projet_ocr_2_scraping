ee020fc45b1a141a21f443d050426b6b513ef7ff
Florent <florentpeyre@hotmail.fr>
2020-11-20 17:17:40 +0100

    creation de la fonction qui récup les urls des book par categories

--- data_recovery.py
+++ data_recovery.py
@@ -5,7 +5,7 @@ from urllib.parse import urljoin
 
 
 def scraping_book_description():
-    url = "https://books.toscrape.com/catalogue/its-only-the-himalayas_981/index.html"
+    url = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
     response = requests.get(url)
     if response.ok:
         soup = BeautifulSoup(response.text, "html.parser")
@@ -27,3 +27,48 @@ def scraping_book_description():
             for key, values in dic_book.items():
                 file.write(key + ':' + values + '\n')
                 print(values)
+
+
+def scraping_book_by_categorie():
+    list_link_book = []
+    list_link_book_by_category = []
+
+    url = "https://books.toscrape.com/catalogue/category/books/sequential-art_5/index.html"
+    response = requests.get(url)
+    if response.ok:
+        soup = BeautifulSoup(response.text, "html.parser")
+        try:
+            button_next = soup.find('li', {'class': 'next'}).find('a')
+        except:
+            pass
+        soup = BeautifulSoup(response.text, "html.parser")
+        urls_book = soup.findAll('div', {'class': 'image_container'})
+        del response
+        for url_book in urls_book:
+            a = url_book.find('a')
+            link_book = a['href']
+            base_url = "http://books.toscrape.com"
+            real_link_book = urljoin(base_url, '/catalogue' + link_book[8:])
+            list_link_book.append(real_link_book)
+        try:
+            while button_next:
+                new_url = url[:-10]
+                button_next = soup.find('li', {'class': 'next'}).find('a')
+                href = button_next['href']
+                url = urljoin(new_url, href)
+                response = requests.get(url)
+                soup = BeautifulSoup(response.text, "html.parser")
+                urls_book = soup.findAll('div', {'class': 'image_container'})
+                del response
+                for url_book in urls_book:
+                    a = url_book.find('a')
+                    link_book = a['href']
+                    base_url = "http://books.toscrape.com"
+                    real_link_book = urljoin(base_url, '/catalogue' + link_book[8:])
+                    list_link_book.append(real_link_book)
+        except:
+            pass
+        list_link_book_by_category.append(list_link_book)
+        print(list_link_book_by_category)
+
+
--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import data_recovery as data
 
 
 def main():
-    data.scraping_book_description()
+    # data.scraping_book_description()
+    data.scraping_book_by_categorie()
 
 
 if __name__ == '__main__':
